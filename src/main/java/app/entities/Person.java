90be9c30f38465a0d04a9e3898e44ecc12dcc993
Morten Jensen <mo_ten@hotmail.com>
2026-02-03 14:50:44 +0100

    Added Person entitie

--- src/main/java/app/entities/Person.java
+++ src/main/java/app/entities/Person.java
@@ -1,4 +1,29 @@
 package app.entities;
 
-public class Person {
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@EqualsAndHashCode
+@ToString
+public class Person
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Setter
+    @Column(name = "name", nullable = false)
+    private String name;
+
+    @Setter
+    @Column(name = "age", nullable = false)
+    private int age;
+
+
 }
