cbef3625dd0d6bc1bbe1f0f7033f2076e5359c1b
Morten Jensen <mo_ten@hotmail.com>
2026-02-03 15:04:56 +0100

    Added first method in PersonDAOImpl create

--- src/main/java/app/Main.java
+++ src/main/java/app/Main.java
@@ -1,8 +1,27 @@
 package app;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        }
+import app.config.HibernateConfig;
+import app.entities.Person;
+import app.persistence.PersonDAO;
+import app.persistence.PersonDAOImpl;
+import jakarta.persistence.EntityManagerFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Logger LOGGER = LoggerFactory.getLogger(Main.class);
+        EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
+        PersonDAO personDAO = new PersonDAOImpl(emf);
+        Person person = Person.builder()
+                .name("Morten")
+                .age(36)
+                .build();
+
+        Person fromDB = personDAO.createPerson(person);
+        LOGGER.info("Testing person from db: {}", fromDB);
+    }
 }
--- src/main/java/app/entities/Person.java
+++ src/main/java/app/entities/Person.java
@@ -24,6 +24,4 @@ public class Person
     @Setter
     @Column(name = "age", nullable = false)
     private int age;
-
-
 }
